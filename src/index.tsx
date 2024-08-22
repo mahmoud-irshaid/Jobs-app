321aa32e8c87dd026279ee761dd4fc657157b719
mahmoud-irshaid <mirshaid10@gmail.com>
2024-08-22 03:00:47 +0300

    Add global css

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ThemeProviderWrapper from "./lib/themeProvider";
+import ThemeProviderWrapper from "./config/themeProvider";
 import "./i18n";
 
 const root = ReactDOM.createRoot(
